0dfcbc977196300b4f90f637a3c4836ff6fad160
Laurie Clark-Michalek <lcm@fb.com>
2018-07-22 12:22:34 +0100

    Add a basic CmdResource
    Cannot support UID or GID without a different binary to run setuid in.
    Pity. However, sudo is available on most systems, and I have no interest
    in adding the complexity of a second binary

---
+++ resource_cmd.go
@@ -0,0 +1,51 @@
+package rfsb
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os/exec"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+// CmdResource execs the specified command when materialized. It will not invoke a shell.
+//
+// For the ability to set UID and GID, see sudo
+type CmdResource struct {
+	ResourceMeta
+
+	Command     string
+	Arguments   []string
+	CWD         string
+	Environment map[string]string
+}
+
+// Materialize runs the specified command
+func (cr *CmdResource) Materialize(ctx context.Context) error {
+	cmd := exec.CommandContext(ctx, cr.Command, cr.Arguments...)
+	cr.Logger().Infof("running %s %s", cr.Command, strings.Join(cr.Arguments, " "))
+	for k, v := range cr.Environment {
+		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", k, v))
+	}
+	if cr.CWD != "" {
+		cmd.Dir = cr.CWD
+	}
+
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+	err := cmd.Run()
+	if err != nil {
+		if _, ok := err.(*exec.ExitError); ok {
+			cr.Logger().Errorf("command stdout:\n%s", stdout.String())
+			cr.Logger().Errorf("command stderr:\n%s", stderr.String())
+			return errors.Wrap(err, "command failed")
+		}
+		return errors.Wrap(err, "could not run command")
+	}
+
+	return nil
+}
